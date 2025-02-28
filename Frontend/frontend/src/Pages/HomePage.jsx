b888f0ed999e190adb290cccf0bd1446cc5128ec
Vansh-Narang <vanshnarang49906@gmail.com>
2025-02-28 10:40:37 +0530

    Added homepage.css

--- Frontend/frontend/src/Pages/HomePage.jsx
+++ Frontend/frontend/src/Pages/HomePage.jsx
@@ -13,17 +13,19 @@ function HomePage() {
             <section className="features-section">
                 <div className="features">
                     <h2>Why Choose Our LMS?</h2>
-                    <div className="feature-card">
-                        <h3>Interactive Learning</h3>
-                        <p>Engage with interactive courses and track your progress.</p>
-                    </div>
-                    <div className="feature-card">
-                        <h3>Efficient Management</h3>
-                        <p>Admins and owners can easily manage users, books, and requests.</p>
-                    </div>
-                    <div className="feature-card">
-                        <h3>Secure and Reliable</h3>
-                        <p>Your data is protected with top-tier security measures.</p>
+                    <div>
+                        <div className="feature-card">
+                            <h3>Interactive Learning</h3>
+                            <p>Engage with interactive courses and track your progress.</p>
+                        </div>
+                        <div className="feature-card">
+                            <h3>Efficient Management</h3>
+                            <p>Admins and owners can easily manage users, books, and requests.</p>
+                        </div>
+                        <div className="feature-card">
+                            <h3>Secure and Reliable</h3>
+                            <p>Your data is protected with top-tier security measures.</p>
+                        </div>
                     </div>
                 </div>
             </section>
